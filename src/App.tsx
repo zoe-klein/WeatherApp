6744ab01229febd33e6ee79e77907f71a3b17ef8
Zoe <you@example.com>
2022-06-02 02:49:15 -0500

    added background and title

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import './Styles.css';
 
 function App() {
   return (
     <div>
+
+      <div className="title-position">
+      <h2 className="title">Weather App</h2>
+      </div>
+
+      <div>
+        {/* adding content here */}
+      </div>
+
     </div>
   );
 }
